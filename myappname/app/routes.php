28e18757012c1a9c8f670ddc5eb60449f211da3b
Juan Pablo Donayre <pablodonayre@gmail.com>
2017-11-27 12:41:52 -0300

    Adding files

---
+++ myappname/app/routes.php
@@ -0,0 +1,15 @@
+<?php
+// Routes
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/', App\Action\HomeAction::class)
+    ->setName('homepage');
+
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
