4d088ef085bf6685abdb56f64c0b3ad8fa8f4ade
Juan Pablo Donayre <pablodonayre@gmail.com>
2018-04-19 02:51:30 -0300

    Fixing database bugs

--- config_mysql/schema.sql
+++ config_mysql/schema.sql
@@ -11,26 +11,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping database structure for course_slim
-CREATE DATABASE IF NOT EXISTS `slim` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `slim`;
-
-
--- Dumping structure for table course_slim.posts
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(11) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `post` varchar(255) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table course_slim.posts: ~2 rows (approximately)
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` (`id`, `title`, `post`, `user_id`) VALUES
-	(1, 'The Slim Framework', 'This is a test', 1),
-	(2, 'Select', 'The select box', 1),
-	(3, 'Controllers', 'The MVC cntroller', 1);
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-
+CREATE DATABASE IF NOT EXISTS `user` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `user`;
 
 -- Dumping structure for table course_slim.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -44,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table course_slim.users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `username`, `name`, `email`, `password`) VALUES
-	(NULL, 'pablodonayre', 'Juan Pablo Donayre', 'pablodonayre@gmail.com', '12345');
+  (NULL, 'pablodonayre', 'Juan Pablo Donayre', 'pablodonayre@gmail.com', '12345');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
